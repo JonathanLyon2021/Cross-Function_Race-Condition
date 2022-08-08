e1aae7d51d28814fd37ba58859839e59221856f9
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-08 11:34:31 -0400

    Update CFRC.sol
    Added public payable function/method deposit.

--- CFRC.sol
+++ CFRC.sol
@@ -3,4 +3,7 @@ pragma solidity ^0.5.1;
 contract CFRC {
     mapping (address => uint) private userBalances;
 
+     function deposit() public payable {
+        userBalances[msg.sender] += msg.value;
+    }
 }
